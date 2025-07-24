33fa121f8689da3044491920200fb3d9b13be4d4
Natnael-Tilahun <natnaeltilahun97@gmail.com>
2025-07-24 11:01:57 +0300

    Fix build error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,7 +33,7 @@ export default defineNuxtConfig({
           __API_BASE_URL__: JSON.stringify(process.env.API_BASE_URL),
           __X_APP_ID__: JSON.stringify(process.env.X_APP_ID),
           __X_APP_VERSION__: JSON.stringify(process.env.X_APP_VERSION),
-          __HOME_URL__: JSON.stringify(process.env.HOME_URL) || "https://cbe-receipt.vercel.app/receipt-generator",
+          __HOME_URL__: JSON.stringify(process.env.HOME_URL),
         },
         plugins: [
           tailwindcss(),
